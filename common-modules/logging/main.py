9fb92db4b38d137b9666913730ed123f3d6673a0
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-05-19 22:59:18 -0500

    logging basic refresh

---
+++ common-modules/logging/main.py
@@ -0,0 +1,33 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename="logs.txt")
+
+
+def process_transactions(user: str, amount: int) -> bool:
+    try:
+        if not user:
+            logging.warning(f"Invalid user provided. Please provide user: {user}")
+            return False
+
+        if not isinstance(user, str):
+            logging.warning("user must be a valid str")
+            return False
+
+        if int(amount) <= 0:
+            logging.warning(f"Amount processing must be greater than 0")
+            return False
+
+        logging.debug(f"transaction of ${amount} for {user} successful")
+
+        return True
+    except ValueError:
+        return False
+    except Exception:
+        logging.exception("Unknown error occurred!")
+        return False
+
+
+user = input("user: ")
+amount = input("amount: ")
+
+process_transactions(user, amount)
