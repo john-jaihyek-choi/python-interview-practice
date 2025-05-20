b031aa08f951184900afd58d76984fb89a28f087
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-05-20 13:42:55 -0500

    commonly used math methods

---
+++ common-modules/math/main.py
@@ -0,0 +1,35 @@
+import math
+
+# ceil number to next integer value (strictly round up)
+ceil = math.ceil(1.12)
+print(ceil)
+
+# floor number to integer value (strictly round down)
+floor = math.floor(1.9)
+print(floor)
+
+# truncates/drops decimal places
+trunc = math.trunc(1.294)
+print(trunc)
+
+# absolute value for float
+absolute = abs(-1.123)
+fabsolute = math.fabs(-1.24313)
+print(fabsolute)
+
+
+# pow
+power = pow(2, 12)
+print(power)
+
+# sqrt
+root = math.sqrt(4)
+print(root)
+
+# gcd
+gcd = math.gcd(12, 13)
+print(gcd)
+
+# lcm
+lcm = math.lcm(2, 5, 6)
+print(lcm)
